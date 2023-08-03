bc4e27747f094a9869c9c9dc8c509ef6ba14671a
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-08-03 14:43:44 +0600

    Create Experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,14 +14,11 @@ import {
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
   carrent,
   jobit,
   tripguide,
   threejs,
+  terra,
 } from "../assets";
 
 export const navLinks = [
@@ -111,55 +108,19 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    title: "Frontend React разработчик",
+    company_name: "TERRA",
+    icon: terra,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    // iconBg: "#E6DEDD",
+    date: "Апрель 2023 - по н.в.",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Разработка элементов интерфейса и использование современных подходов для управления состоянием приложения на стеке JavaScript, ReactJS, Redux Toolkit, TypeScript, HTML, SCSS.",
+      "Рефакторинг кода с целью оптимизации вычислений.",
+      "Адаптивная, кроссбраузерная, семантичная и валидная вёрстка.",
+      "Чтение и модернизация чужого кода.",
+      "Планирование и контроль сроков, декомпозиция задач.",
+      "Предложение архитектурных решений.",
     ],
   },
 ];
